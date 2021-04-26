502c809b02b433cbe43ec04608d30de957cbff1e
sckelrt <74903900+sckelrt@users.noreply.github.com>
2021-04-26 17:52:33 +0900

    Update 3_2.py
    становится все хуже

--- 3_2.py
+++ 3_2.py
@@ -14,9 +14,14 @@ def member():
     with open("templates/data.json", "rt", encoding="utf8") as f:
         data_list = json.loads(f.read())
         for data in data_list:
-            if aquariumParametres[data][0] < data < aquariumParametres[data][1]:
-                RGBparamemtres.append(f'0.{int(data) * 3}.77')
-    return render_template('member.html', data_list=data_list)
+            try:
+                if aquariumParametres[data][0] < float(data_list[data]) < aquariumParametres[data][1]:
+                    RGBparamemtres.append("alert-success")
+                else:
+                    RGBparamemtres.append("alert-danger")
+            except:
+                print('false')
+    return render_template('member.html', data_list=data_list, RGBparamemtres=RGBparamemtres)
 
 
 @app.route('/')
