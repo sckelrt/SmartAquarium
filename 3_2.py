666c9e27f13448f63510f54d6f159e1872dd28c0
zelcamn <zelcamn@yandex.ru>
2021-04-26 17:11:28 +0900

    RGB if

--- 3_2.py
+++ 3_2.py
@@ -4,11 +4,18 @@ from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
+aquariumParametres = {'temp': (27, 30), 'humidity': (90, 99), 'temp2': (24, 26), 'PH': (6, 8.5), 'volt': (1, 2),
+                      'light': (100, 200)}
+
 
 @app.route('/data')
 def member():
+    RGBparamemtres = list()
     with open("templates/data.json", "rt", encoding="utf8") as f:
         data_list = json.loads(f.read())
+        for data in data_list:
+            if aquariumParametres[data][0] < data < aquariumParametres[data][1]:
+                RGBparamemtres.append(f'0.{int(data) * 3}.77')
     return render_template('member.html', data_list=data_list)
 
 
@@ -18,4 +25,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    app.run(port=8081, host='127.0.0.1')
