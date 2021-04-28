7e4570faac4cee585425054ed96b063d78473a31
zelcamn <zelcamn@yandex.ru>
2021-04-28 16:46:27 +0900

    made db

--- 3_2.py
+++ 3_2.py
@@ -1,12 +1,28 @@
 import json
-
+from data import db_session
 from flask import Flask, render_template, redirect
+from data.SavingData import SavingData
 
+
+SavingDataManager = SavingData()
 app = Flask(__name__)
 
 aquariumParametres = {'temp': (27, 30), 'humidity': (90, 99), 'temp2': (24, 26), 'PH': (6, 8.5), 'volt': (1, 2),
                       'light': (100, 200)}
 
+def dataSave(data):
+    SavingDataManager.temp_air = data[0]
+    SavingDataManager.humidity = data[1]
+    SavingDataManager.temp_water = data[2]
+    SavingDataManager.PH = data[3]
+    SavingDataManager.volt = data[4]
+    SavingDataManager.water = data[5]
+    SavingDataManager.light = data[6]
+    SavingDataManager.LIGH = data[7]
+    SavingDataManager.Cooling = data[8]
+    db_sess = db_session.create_session()
+    db_sess.add(SavingDataManager)
+    db_sess.commit()
 
 @app.route('/')
 def index():
@@ -15,6 +31,7 @@ def index():
 
 @app.route('/data')
 def data():
+    DataContanier = []
     RGBparamemtres = list()
     with open("templates/data.json", "rt", encoding="utf8") as f:
         data_list = json.loads(f.read())
@@ -22,10 +39,14 @@ def data():
             try:
                 if aquariumParametres[data][0] < float(data_list[data]) < aquariumParametres[data][1]:
                     RGBparamemtres.append("alert-success")
+                    DataContanier.append(data_list[data])
                 else:
                     RGBparamemtres.append("alert-danger")
+                    DataContanier.append(data_list[data])
             except:
                 print('false')
+                DataContanier.append(data_list[data])
+    dataSave(DataContanier)
     return render_template('data.html', data_list=data_list, RGBparamemtres=RGBparamemtres)
 
 
@@ -35,4 +56,5 @@ def news():
 
 
 if __name__ == '__main__':
+    db_session.global_init("db/AquariumData.db")
     app.run(port=8081, host='127.0.0.1')
---
+++ data/SavingData.py
@@ -0,0 +1,21 @@
+import datetime
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+
+
+class SavingData(SqlAlchemyBase):
+    __tablename__ = 'SavingData'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    temp_air = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    humidity = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    temp_water = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    PH = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    volt = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    water = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    light = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    LIGH = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    Cooling = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.datetime.now)
